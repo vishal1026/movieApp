aefb940ddb9b261df2b6268ce5981991b1f40f32
vishal <vishalkhandekar1026@gmail.com>
2019-08-28 12:58:12 +0530

    setup

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movieApp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'movie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movieApp',
+        'USER':'root',
+        'PASSWORD': '1',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
