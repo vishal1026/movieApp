fe21588449e86cc5ba850c288e55c63c7c1f1515
vishal <vishalkhandekar1026@gmail.com>
2019-08-28 16:01:32 +0530

    database created

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movieApp'
+    'rest_framework',
+    'movieApp',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- movie/movieApp/models.py
+++ movie/movieApp/models.py
@@ -1,6 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Movie_user(models.Model):
+	user_id = models.AutoField(primary_key=True)
+	user_name = models.CharField(max_length=70, unique = True)
+	password = models.CharField(max_length=70)
+	is_active = models.IntegerField()
+	user_type = models.IntegerField(null=False)
+	created_on = models.DateField(default=datetime.now)
+	last_login = models.DateField(default=datetime.now)
+
+	class Meta:
+		db_table = 'movie_user'
+
+class Common_create(models.Model):
+    created_on = models.DateField(default=datetime.now)
+    created_by = models.ForeignKey( Movie_user)
+
+    class Meta:
+        abstract = True
+
+class Genre(Common_create):
+	genre_id  = models.AutoField(primary_key=True)
+	genre_type = models.CharField(max_length=50, unique = True)
+
+	class Meta:
+		db_table = 'genre'
+
+class Artist(Common_create):
+	artist_id  = models.AutoField(primary_key=True)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'artist'
+
+
+class Movie(Common_create):
+	movie_id  = models.AutoField(primary_key=True)
+	movie_name = models.CharField(max_length=100)
+	description = models.TextField(blank=True)
+	genre_id  = models.ManyToManyField(Genre)
+	artist_id = models.ManyToManyField(Artist)
+	release_date = models.DateField()
+
+	class Meta:
+		db_table = 'movie'
